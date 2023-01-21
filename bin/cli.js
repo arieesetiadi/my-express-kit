1ad9d489577a2f0f00baf12b77d150d5332f809b
Arie Setiadi <ariesetiadi.sm@gmail.com>
2023-01-21 20:22:00 +0800

    Add 'Degit' command to remote .git directory

--- bin/cli.js
+++ bin/cli.js
@@ -1,24 +1,29 @@
 #!/usr/bin/env node
 
+import fs from "fs";
 import { execSync } from "child_process";
 
 const runCommand = (command) => {
     try {
         execSync(command, { stdio: "inherit" });
     } catch (error) {
-        console.error(`Failed to execute ${command}`, e);
+        console.error(`Failed to execute ${command}`, error);
         return false;
     }
     return true;
 };
 
 const repoName = process.argv[2];
-const cloneCommand = `git clone --depth 1 https://github.com/ariesetiadi-sm/my-express-kit ${repoName}`;
 
 // Cloning command
 console.log(`Cloning the repository  with name ${repoName}`);
+const cloneCommand = `git clone --depth 1 https://github.com/ariesetiadi-sm/my-express-kit.git ${repoName}`;
 const cloned = runCommand(cloneCommand);
 if (!cloned) process.exit(-1);
 
-console.log("Congratulations! Your express kit is ready");
-console.log("Have a nice day ⭐");
+// Degit / remove .git directory
+const degitCommand = `rm -rf !$/.git`;
+const degit = runCommand(degitCommand);
+if (!degit) process.exit(-1);
+
+console.log("Congratulations! Your express kit is ready, have a nice day :)");
